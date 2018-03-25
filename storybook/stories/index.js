45387d79beed366d1f6e406a43259d3dc0f69a48
César Alberca <cesalberca@gmail.com>
2018-03-25 19:11:04 +0200

    Remove slot content

--- storybook/stories/index.js
+++ storybook/stories/index.js
@@ -4,4 +4,4 @@ import { storiesOf } from '@storybook/react';
 import '../components/components.js'
 
 storiesOf('Button', module)
-  .add('with text', () => <au-button>Hello</au-button>)
+  .add('with text', () => <au-button></au-button>)
